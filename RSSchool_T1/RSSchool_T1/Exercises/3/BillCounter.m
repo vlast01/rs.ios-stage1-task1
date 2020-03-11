2a15d8a1c185913da32f0f75bd7e328be3ac9be9
Vladislav Stankevich <rct.stankevivs@gmail.com>
2020-03-11 22:57:23 +0300

    feat: add decision of the 3rd task

--- RSSchool_T1/RSSchool_T1/Exercises/3/BillCounter.m
+++ RSSchool_T1/RSSchool_T1/Exercises/3/BillCounter.m
@@ -4,9 +4,21 @@
 
 // Complete the following fuction
 - (NSString*)compareResultForBill:(NSArray<NSNumber*>*)bill notIncludingElementWithIndex:(NSInteger)index withGivenSum:(NSNumber*)sum {
+   
+    NSInteger totalPrice = 0;
+    for (NSNumber *num in bill) {
+      totalPrice += [num intValue];
+    }
     
-  
-    return @"";
+    NSInteger correctBill = totalPrice - [[bill objectAtIndex:index] intValue];
+    
+    if ([sum intValue] == correctBill/2) {
+        return @"Bon Appetit";
+    }
+    else {
+        NSInteger refund = [sum intValue] - correctBill/2;
+        return [NSString stringWithFormat: @"%ld",(long)refund];
+    }
 }
 
 @end
